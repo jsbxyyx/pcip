52d87205cee8181db12b98a53581c9087264fb6c
jsbxyyx <jsbxyyx@163.com>
2025-02-11 10:09:16 +0800

    okhttp

--- src/test/java/org/op/Tests.java
+++ src/test/java/org/op/Tests.java
@@ -1,14 +1,28 @@
 package org.op;
 
+import okhttp3.Dns;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
 
 import java.io.DataOutputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.URI;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Tests {
 
@@ -16,31 +30,55 @@ public class Tests {
     public void test() throws Exception {
         String str = new Main().run(new String[]{"ip"});
 
-        URL url = new URL(new String(Base64.getDecoder().decode("aHR0cHM6Ly9tcC53eHMudXMua2cvY3A="), StandardCharsets.UTF_8));
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("POST");
-        conn.setRequestProperty("content-type", "text/plain");
-        conn.setRequestProperty("content-length", str.getBytes(StandardCharsets.UTF_8).length + "");
-        conn.setUseCaches(false);
-        conn.setDoOutput(true);
+        if (str == null || str.isBlank()) {
+            System.out.println("str is blank");
+            return;
+        }
 
-        DataOutputStream out = new DataOutputStream(conn.getOutputStream());
-        out.write(str.getBytes(StandardCharsets.UTF_8));
-        out.flush();
-        out.close();
+        final String h = new String(Base64.getDecoder().decode("dGVzdC50ZWNoaW9pby5ldS5vcmc="), StandardCharsets.UTF_8);
 
-        InputStream input = conn.getInputStream();
-        String string = IOUtils.toString(input, StandardCharsets.UTF_8);
-        System.out.println(string);
-        input.close();
+        String[] hs = StringUtils.split(str, "\n");
 
-        conn.disconnect();
+        OkHttpClient.Builder builder = new OkHttpClient.Builder()
+                .connectTimeout(5000, TimeUnit.MILLISECONDS)
+                .readTimeout(5000, TimeUnit.MILLISECONDS)
+                .writeTimeout(5000, TimeUnit.MILLISECONDS);
+        builder.setDns$okhttp(new Dns() {
+            @NotNull
+            @Override
+            public List<InetAddress> lookup(@NotNull String s) throws UnknownHostException {
+                if (h.equals(s)) {
+                    if (hs != null && hs.length > 0) {
+                        List<InetAddress> addresses = new ArrayList<>();
+                        for (String string : hs) {
+                            if (string != null && !string.isBlank()) {
+                                addresses.add(InetAddress.getByName(string));
+                            }
+                        }
+                        return addresses;
+                    }
+                }
+                return SYSTEM.lookup(s);
+            }
+        });
+        OkHttpClient client = builder.build();
+
+        Response response = client.newCall(new Request.Builder()
+                        .url("https://" + h + "/cp")
+                        .addHeader("content-type", "text/plain")
+                        .post(RequestBody.create(str, MediaType.parse("text/plain")))
+                .build()).execute();
+        String responseString = response.body().string();
+        System.out.println(responseString);
     }
 
     @Test
     public void test2() throws Exception {
         System.out.println(
 
+        );
+        System.out.println(
+                new String(Base64.getDecoder().decode("dGVzdC50ZWNoaW9pby5ldS5vcmc=".getBytes(StandardCharsets.UTF_8)))
         );
     }
 
